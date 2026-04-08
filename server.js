6820d40f2bac6590bbe9e558e9a18179a4a3144e
Vimal Chaudhary <vimal07chaudhary@gmail.com>
2026-04-08 14:35:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'assets')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
